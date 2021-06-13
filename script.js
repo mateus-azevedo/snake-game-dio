bdd7ae7fef45a3d35c2152be3eed2c2e31e3bc2b
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-13 15:44:48 -0300

    Logica Game Over

--- script.js
+++ script.js
@@ -47,6 +47,13 @@ function iniciarJogo() {
   else if (snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
   else if (snake[0].y < 0 * box && direction === "up") snake[0].y = 16 * box;
 
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      clearInterval(jogo);
+      alert("Game Over ;D");
+    }
+  }
+
   criarBG();
   criarCobrinha();
   drawFood();
