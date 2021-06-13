5f879ea9aeef0c4297a57312fcb087bcf2ddaa6b
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-13 15:19:38 -0300

    definindo direcoes e limitacoes da cobrinha

--- script.js
+++ script.js
@@ -21,9 +21,23 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode === 37 && direction != "right") direction = "left";
+  else if (event.keyCode === 38 && direction != "down") direction = "up";
+  else if (event.keyCode === 39 && direction != "left") direction = "right";
+  else if (event.keyCode === 40 && direction != "up") direction = "down";
+}
+
 let direction = "right";
 
 function iniciarJogo() {
+  if (snake[0].x > 15 * box && direction === "right") snake[0].x = 0;
+  else if (snake[0].x < 0 * box && direction === "left") snake[0].x = 16 * box;
+  else if (snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
+  else if (snake[0].y < 0 * box && direction === "up") snake[0].y = 16 * box;
+
   criarBG();
   criarCobrinha();
 
